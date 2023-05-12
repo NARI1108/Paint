d5dcda3ef40cb74ccef654431b375b09ab3f8225
Hill <125409221+NARI1108@users.noreply.github.com>
2023-05-12 12:24:51 +0430

    Creating a method for sorting findViewById() at goat_Activity.java

--- app/src/main/java/com/example/paint/goat_Activity.java
+++ app/src/main/java/com/example/paint/goat_Activity.java
@@ -2,13 +2,21 @@ package com.example.paint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class goat_Activity extends AppCompatActivity {
-
+    ImageView img_Music;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
+
+        findViews();
     }
+     public void findViews(){
+         img_Music = findViewById(R.id.img_Music);
+     }
 }
